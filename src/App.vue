e13ff4d93339441deb4c061e531d2f2e6c08c044
arsad33 <mustafa.aang@gmail.com>
2022-01-07 19:45:03 +0300

    second-commit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-import Left from "./Left.vue"
 <template>
   <div class="container">
     <div class="container-inner">
@@ -9,20 +8,21 @@ import Left from "./Left.vue"
           <h2>QA/QC</h2>
         </div>
         <div class="form">
-          <form>
+          <form @submit="onClick" method="post">
             <div class="name">
                 <label for="username">Username</label>
-                <input type="text" name="username" id="username">
+                <input type="text" name="username" id="username" v-model="username">
             </div>
             <div class="password">
                 <label for="passwd">Password</label>
-                <input type="password" name="passwd" id="passwd">
+                <input type="password" name="passwd" id="passwd" v-model="password">
             </div>
-              <input type="button" value="LOGIN">
+              <button type="submit">LOGIN</button>
           </form>
           <div class="footer">
               <img src="./assets/logo.png" alt="star-logo" class="logo">
               <h3 class="layernote">LAYER<span>NOTE</span></h3>
+              <p id="status"></p>
           </div>
         </div>
       </div>
@@ -35,8 +35,21 @@ import Left from "./Left.vue"
 </template>
 
 <script>
+import axios from "axios";
 export default {
-
+  data(){
+    return{
+      username: "",
+      password: "",
+    };
+  },
+  methods:{
+    onClick(){
+      axios.post("/", {username: this.username, password: this.password}).then(response =>{
+        console.log(response);
+      })
+    }
+  }
 }
 </script>
 
@@ -53,7 +66,7 @@ form>*{margin-bottom: 20px;}
 label{float:left; font-size: 15px; letter-spacing: 0px;}
 input[type=text]{box-sizing: border-box; padding: 10px; margin: 10px 0; border: 1px solid grey; border-radius: 8px; width: 100%; background-color: rgba(100, 148, 237, 0.274);}
 input[type=password]{box-sizing: border-box; padding: 10px; margin: 10px 0; border: 1px solid grey; border-radius: 8px; width: 100%; background-color:rgba(100, 148, 237, 0.274);}
-input[type=button]{padding: 10px; border: none; background-color: #3b54c5; color: white; width: 100%; border-radius: 10px; margin-bottom: 50px;}
+button{padding: 10px; border: none; background-color: #3b54c5; color: white; width: 100%; border-radius: 10px; margin-bottom: 50px;}
 .container{background-color:rgb(41, 38, 38); height: 100vh; display: flex; align-items: center;}
 .container-inner{height: 85vh; width: 75%; display:grid; grid-template-columns: 1fr 2fr; background-color: white; margin: auto auto; border-radius: 10px;}
 .container-left{ text-align: center; margin: 20%; display: inline-block; height: 100%; margin-top: 10px;}
